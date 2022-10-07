160ff8bd8b2a7324f1776531672f8e5ecd804be7
Temidayo David <111475450+Temmy009@users.noreply.github.com>
2022-10-07 05:19:48 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsTemmy009',
-	    'sonar.login': '41c82131519e174fcd60aef2b8072cf0da4a5bc9',
+	    'sonar.login': '4ed74032996a613e094b9fe8cab73828697351db',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
